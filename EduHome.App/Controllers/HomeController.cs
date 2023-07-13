638bd0ad41e681eee0902d695b7401dc308ecef5
İlkin <ilkinhd@code.edu.az>
2023-07-13 22:39:21 +0400

    FronttoBack Start

---
+++ EduHome.App/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EduHome.App.Controllers
+{
+	public class HomeController : Controller
+	{
+		public async Task<IActionResult> Index()
+		{
+			return View();
+		}
+	}
+}
