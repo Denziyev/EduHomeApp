ffb6d14d843c190a89c8a97e6f2bc9f2b4f09841
İlkin <ilkinhd@code.edu.az>
2023-07-18 00:09:28 +0400

    Login Page is added

---
+++ EduHome.App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduHome.App.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+       public bool isRememberMe { get; set; }
+
+    }
+}
