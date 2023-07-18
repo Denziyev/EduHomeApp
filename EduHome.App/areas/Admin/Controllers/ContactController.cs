bcb5f76fcd08154dce4f3417519b8c435f0da337
İlkin <ilkinhd@code.edu.az>
2023-07-18 17:58:14 +0400

    Send Message is added

---
+++ EduHome.App/areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,41 @@
+﻿using EduHome.App.Context;
+using EduHome.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHome.App.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ContactController : Controller
+    {
+        private readonly EduHomeAppDxbContext _context;
+
+        public ContactController(EduHomeAppDxbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            IEnumerable<Message> messages = await _context.Messages.Where(x => !x.IsDeleted).ToListAsync();
+            return View(messages);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Message? message = await _context.Messages.Where(x => !x.IsDeleted && x.Id == id).FirstOrDefaultAsync();
+            if (message == null)
+            {
+                return NotFound();
+            }
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            message.IsDeleted = true;
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Contact");
+        }
+    }
+}
